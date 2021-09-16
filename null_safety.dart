ce37c678840767a0af54b243618bc1ed1ed8436d
ivan-boginski <aisivan36@gmail.com>
2021-09-16 17:16:39 +0700

    null safety

--- null_safety.dart
+++ null_safety.dart
@@ -7,4 +7,25 @@ void main() {
   print('aList is: $aList');
   print('aNullableListOfString is: $aNullableListOfString');
   print('aListOfNullableString is: $aListOfNullableString');
+  nullAsertion();
+}
+
+///  If you’re wrong, Dart throws an exception at `run-time`. This makes the `!` operator `unsafe`, so don’t use it unless you’re very sure that the expression isn’t null.
+int? couldReturnNullButDoesnt() => -3;
+
+void nullAsertion() {
+  int? couldReturnNullButIsnot = 1;
+  List<int?> listThatCouldHoldNulls = [2, null, 4];
+
+  int a = couldReturnNullButIsnot;
+
+  /// mark with ! on the first item on the list
+  int b = listThatCouldHoldNulls.first!;
+
+  /// abs absolute value
+  int c = couldReturnNullButDoesnt()!.abs();
+
+  print('a is $a');
+  print('b is $b');
+  print('c is $c');
 }
