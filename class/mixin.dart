c1e8e2e1b18bf9c145680f8f68e5921a557c5016
ivan-boginski <aisivan36@gmail.com>
2021-10-03 08:41:30 +0700

    mixin

--- class/mixin.dart
+++ class/mixin.dart
@@ -1,12 +1,48 @@
+import 'dart:html';
+
 class Animal {
   void move() {
     print('changed posistion');
   }
 }
 
-class Fish extends Animal {}
+class Fish extends Animal {
+  @override
+  void move() {
+    super.move(); // Override method super.move()
+    print('By Swiming');
+  }
+}
+
+class Bird extends Animal {
+  @override
+  void move() {
+    // super to call the original value from Animal class then then override with
+    // other method statement
+    super.move(); // Override method super.move()
+    print('By Flying');
+  }
+}
+
+mixin CanSwim {
+  void swim() {
+    print('Changing position by swiming');
+  }
+}
+
+mixin CanFly {
+  void fly() {
+    print('Changing position by flying');
+  }
+}
+
+class Duck extends Animal with CanSwim, CanFly {}
+
+class Airplane with CanFly {}
 
 void main() {
-  Animal().move();
+  // Animal().move();
   Fish().move();
+  Bird().move();
+  Duck().fly();
 }
