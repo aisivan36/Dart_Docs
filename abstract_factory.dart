0e372f094c6652a9be190269466aee194bae1864
ivan-boginski <aisivan36@gmail.com>
2021-10-06 17:23:54 +0700

    abstract factory

---
+++ abstract_factory.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+abstract class Shape {
+  factory Shape({String? type}) {
+    if (type == 'circle') return Circle(radius: 2);
+    if (type == 'square') return Square(side: 2);
+
+    /// To trigger exception, don't implement a check for 'triangle'.
+    throw 'Can\'t create $type';
+  }
+  num get area;
+}
+
+class Circle implements Shape {
+  final num? radius;
+  Circle({this.radius});
+  @override
+  num get area => pi * pow(radius!, 2);
+}
+
+class Square implements Shape {
+  final num? side;
+  Square({this.side});
+  @override
+  num get area => pow(side!, 2);
+}
+
+class Triangle implements Shape {
+  final num? side;
+  Triangle({this.side});
+  @override
+  num get area => pow(side!, 2) / 2;
+}
+
+main() {
+  try {
+    print(Shape(type: 'circle').area);
+    print(Shape(type: 'square').area);
+    print(Shape(type: 'Triangle').area);
+  } catch (err) {
+    print(err);
+  }
+}
