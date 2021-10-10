7c36bfd468821936d6ea52fd99b6f41099bbe61a
ivan-boginski <aisivan36@gmail.com>
2021-10-10 08:00:29 +0700

    Switch Case

---
+++ switch_case.dart
@@ -0,0 +1,23 @@
+void main() {
+  const number = 3;
+
+  switch (number) {
+    case 0:
+      print('zero');
+      break;
+    case 1:
+      print('one');
+      break;
+    case 2:
+      print('two');
+      break;
+    case 3:
+      print('three');
+      break;
+    case 4:
+      print('four');
+      break;
+    default:
+      print('Something else');
+  }
+}
