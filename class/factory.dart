1d89783eec79c02f49d56003c7c8965c94fb2670
ivan-boginski <aisivan36@gmail.com>
2021-10-20 03:19:10 +0700

    factory

--- class/factory.dart
+++ class/factory.dart
@@ -8,6 +8,11 @@ class User {
     final userName = json['name'] as String;
     return User(id: userId, name: userName);
   }
+
+  /// If not using factory
+  User.fromJsonAlready(Map<String, dynamic> json)
+      : id = json['id'] as int,
+        name = json['name'] as String;
 }
 
 void main(List<String> args) {
@@ -15,3 +20,39 @@ void main(List<String> args) {
   final derived = User.fromJson(ivan);
   print('id: ${derived.id}\nname: ${derived.name}');
 }
+
+
+
+//// However, besides adding some simple asserts to
+///the initializer list, there isn't much else you can
+///do to with id and name. With a factory
+///constructor, though, you could do all kinds of
+///validation, error checking and even modification
+///of the arguments before creating the object. This
+///is actually highly desireable in the case here
+///because if 'id' or 'String' didn't exist in the
+///map, then your app would crash because you
+///aren't handling null.
+///
+///
+///
+/// [Note]: Using a factory constructor over a
+///named constructor can also help to prevent
+///breaking changes for subclasses of your
+///class. That topic is a little beyond the scope
+///of this chapter, but you can read
+///[https://stackoverflow.com/a/66117859] for a
+///longer explanation.
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
